2ead96b25e3ea69a785c732caaf5e3431bfb2fe8
Ceddy Muhoza <ceddymuhoza@gmail.com>
2020-04-27 02:01:37 -0400

    mille events

---
+++ src/MilleEvents.ts
@@ -0,0 +1,21 @@
+import EventEmitter from 'events';
+
+export enum MILLEEVENTS {
+    DATA = 'data',
+}
+
+export class MilleEvents extends EventEmitter.EventEmitter {
+    private cache = {};
+    private static _instance: MilleEvents;
+
+    public static get Instance() {
+        return this._instance || (this._instance = new this());
+    }
+
+    private constructor() {
+        super()
+        // this.setMaxListeners(50); // set a maximum of 50 event listners
+    }
+
+}
+
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,11 @@
-require('dotenv').config();
+// Envs
+export const isDev = process.env.NODE_ENV !== 'production';
 
-const { env }  = process;
+
+if (isDev) {
+    require('dotenv').config();
+}
+
+const { env } = process;
 
 export const FINNHUB_KEY = env.FINNHUB_KEY;

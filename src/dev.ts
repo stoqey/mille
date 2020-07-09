40f6680c17d95099e7439131b0ff5dd844031867
Ceddy Muhoza <ceddymuhoza@gmail.com>
2020-07-09 15:18:16 -0400

    version 8

--- src/dev.ts
+++ src/dev.ts
@@ -2,6 +2,10 @@ import { mille, MILLEEVENTS, MilleEvents } from ".";
 
 const milleEvents = MilleEvents.Instance;
 
-mille();
+mille({
+    startDate: new Date("2020-03-11 10:35:00"),
+    endDate: new Date("2020-03-11 16:35:00"),
+    mode: 'secs',
+});
 
 milleEvents.emit(MILLEEVENTS.GET_DATA, ["AAPL", "MSFT"])
